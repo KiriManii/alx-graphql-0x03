fa279d764770651ced7f3da6887b5302521dd494
Lewis Kimani <lkirigwi@gmail.com>
2025-08-24 12:26:51 -0400

    feat: Wrap application with ErrorBoundary in _app.tsx

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -1,13 +1,16 @@
-    import "@/styles/globals.css";
-    import type { AppProps } from "next/app";
-    import { ApolloProvider } from "@apollo/client";
-    import client from "@/graphql/apolloClient";
+import ErrorBoundary from '@/components/ErrorBoundary'; // This line brings in our ErrorBoundary
+import type { AppProps } from "next/app";
 
-    export default function App({ Component, pageProps }: AppProps) {
-      return (
-        <ApolloProvider client={client}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      )
-    }
-    
+// This is the main function that wraps all your Next.js pages.
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    // We wrap the entire application (represented by <Component {...pageProps} />)
+    // with our ErrorBoundary. Now, if any error happens inside any page or component
+    // within our app, the ErrorBoundary will catch it!
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
